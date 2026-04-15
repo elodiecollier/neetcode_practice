53a38379221e3d0429704414de2ac1d39357877d
Elodie Collier <elodiecollier@Elodies-Air.lan>
2026-04-14 20:49:59 -0400

    Initial set up for neetcode practice problems

---
+++ neetcode/neetcode/neetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  neetcodeApp.swift
+//  neetcode
+//
+//  Created by Elodie Collier on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct neetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
